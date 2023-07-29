6180d8544a9dbc78e824ddc6ec07239fd48a6c3a
TgSBot_xD <falleytrd88@gmail.com>
2023-07-29 19:01:26 +0500

    Change Folder

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,74 +1,11 @@
-import React, { FC } from 'react';
-import { styled } from 'styled-components';
-import Navbar from '../components/Navbar';
-import Text from '../UI/Text/Text';
-import Button from '../UI/Button/Button';
-import BlockText from '../UI/BlockText/BlockText';
+import React from 'react';
+import Header from '../modules/Header';
 
-const Wrapper = styled.div`
-	width: 100%;
-	height: 100%;
-	display: flex;
-	background-color: #484283;
-	justify-content: center;
-`;
-
-const Row = styled.div`
-	width: fit-content;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	margin: 20px 0px 100px 0px;
-	z-index: 1;
-`;
-
-const TransparentInscription = styled.div`
-	position: absolute;
-	top: 217px;
-	right: 260px;
-	z-index: 0;
-	width: 980px;
-	height: 200px;
-`;
-
-const HomePage: FC = () => {
+const HomePage = () => {
 	return (
-		<Wrapper>
-			<TransparentInscription>
-				<Text
-					fontFamily='Intro-Bold'
-					fontSize='196px'
-					fontWeight='700'
-					color='rgba(255, 255, 255, 0.12)'
-				>
-					SneakMax
-				</Text>
-			</TransparentInscription>
-			<Row>
-				<Navbar />
-				<BlockText width='580px' height='100px' margin='100px 0px 20px 0px'>
-					<Text fontFamily='Intro-Regular' fontSize='36px' line_height='50.4px'>
-						Кроссовки известных брендов с доставкой по России и СНГ
-					</Text>
-				</BlockText>
-				<BlockText width='480px' height='44px' margin='0px 0px 40px 0px'>
-					<Text fontFamily='Intro-Book' fontSize='16px' line_height='22.4px'>
-						Мы продаем кроссовки брендов Nike, Adidas, Puma, Reebok, Converse и
-						многие другие по низким ценам
-					</Text>
-				</BlockText>
-				<Button
-					ground_color='#F14F4F'
-					width='250px'
-					height='60px'
-					border_radius='4px'
-				>
-					<Text fontFamily='Intro-Regular' fontSize='16px' color='white'>
-						Перейти к покупкам
-					</Text>
-				</Button>
-			</Row>
-		</Wrapper>
+		<>
+			<Header />
+		</>
 	);
 };
 
